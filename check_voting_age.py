33e45e85f0a3cf65240ce26762b98e11540a0549
deepak chauhan <112190394+Deepakdschauhan@users.noreply.github.com>
2022-11-19 20:31:15 +0530

    Create check_voting_age.py

---
+++ check_voting_age.py
@@ -0,0 +1,10 @@
+age = input("enter your age as adhar : ")
+#lets check some conditions....
+if  18 <= int(age) <= 70:
+    print("HURRAYY! you are eligible for vote..")
+elif int(age) > 70:
+    print("you are about to die.. why are you thinking about vote..hahaha")
+    # this line was for joke hehehehehe
+else:
+    print(" sorry! you cant vote. you are not an adult.")
+    # so lets run ....
