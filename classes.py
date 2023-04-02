5aafb5129a9a913b38ddf471a37b73587c0ca62e
deepak chauhan <112190394+Deepakdschauhan@users.noreply.github.com>
2023-04-02 11:03:46 +0530

    Update and rename classes.py to list comprehension.py

--- classes.py
+++
@@ -1,7 +0,0 @@
-a= []
-i=0
-while i<11:
-    i= i+1
-    a.append(i)
-   
-print(a)
---
+++ list comprehension.py
@@ -0,0 +1,10 @@
+#list by function
+a= []
+i=0
+while i<11:
+    i= i+1
+    a.append(i)
+   
+print(a)
+#list by list comprehension....
+a=[x for x in range(20)]
