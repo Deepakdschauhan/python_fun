6967a75603775e23a692a1d4a02294bb3b69fee6
deepak chauhan <112190394+Deepakdschauhan@users.noreply.github.com>
2023-04-02 11:06:31 +0530

    Update and rename find.py to find in your name.py

---
+++ find in your name.py
@@ -0,0 +1,3 @@
+# first enter your name and search any letter index in your name..
+name = input("ENTER YOUR NAME : ")
+print(name.find(input("enter alphabet you want to find in your name : ")))
--- find.py
+++
@@ -1,2 +0,0 @@
-name = input("ENTER YOUR NAME : ")
-print(name.find(input("enter alphabet you want to find in your name : ")))
